1a605d95681681940d356366433337bb77cb1df9
hafidhsajid <hafidhsajid@gmail.com>
2023-02-10 16:55:07 +0700

    try calling inheritance program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace ConsoleUI // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<Car> listcar = new List<Car>();
+            listcar.Add(new Toyota());
+            listcar.Add(new Hyundai());
+            foreach (var item in listcar)
+            {
+                item.getMerk();
+            }
+
+        }
+    }
+}
